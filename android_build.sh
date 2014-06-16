1ee8f3914d793d5f9c10a1f1ec7ea08315b4cd8a
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2014-06-16 06:27:04 -0400

    Updated docs

--- android_build.sh
+++ android_build.sh
@@ -9,21 +9,46 @@ export PATH=${ANDROID_NDK_ROOT_PATH}:${ANDROID_NDK_ROOT_PATH}/toolchains/arm-lin
 NDK_SYSROOT=$NDK_BASE/platforms/$ANDROID_API_VERSION/arch-arm
 NUMBER_OF_CORES=$(nproc) # or number of cores you want to use to compile ffmpeg
 
-## Donot touch anything below this line
-#x264
-pushd x264
-make distclean
-
+# Basic compilation in linux
+# ./configure -
 # This command makes the shell run the script named configure which exists in the current directory. The configure script basically 
 # consists of many lines which are used to check some details about the machine on which the software is going to be installed. 
 # This script checks for lots of dependencies on your system. For the particular software to work properly, it may be requiring a 
 # lot of things to be existing on your machine already.  When you run the configure script you would see a lot of output on the screen, 
 # each being some sort of question and a respective yes/no as the reply. If any of the major requirements are missing on your system, 
 # the configure script would exit and you cannot proceed with the installation, until you get those required things. 
-
 # The main job of the configure script is to create a Makefile. This is a very important file for the installation process. Depending on 
 # the results of the tests (checks) that the configure script performed it would write down the various steps that need to be taken (while 
 # compiling the software) in the file named Makefile.
+
+# make
+# make is actually a utility which exists on almost all Unix systems. For make utility to work it requires a file named Makefile in the same 
+# directory in which you run make. As we have seen the configure script's main job was to create a file named Makefile to be used with make 
+# utility. (Sometimes the Makefile is named as makefile also)
+# make would use the directions present in the Makefile and proceed with the installation. The Makefile indicates the sequence, that Linux 
+# must follow to build various components / sub-programs of your software. The sequence depends on the way the software is designed as well 
+# as many other factors.
+# The Makefile actually has a lot of labels (sort of names for different sections). Hence depending on what needs to be done the control would 
+# be passed to the different sections within the Makefile Or it is possible that at the end of one of the section there is a command to go to 
+# some next section.
+# Basically the make utility compiles all your program code and creates the executables. For particular section of the program to complete might 
+# require some other part of the code already ready, this is what the Makefile does. It sets the sequence for the events so that your program does 
+# not complain about missing dependencies.
+
+# make install
+# As indicated before make uses the file named Makefile in the same directory. When you run make without any parameters, the instruction in the 
+# Makefile begin executing from the start and as per the rules defined within the Makefile (particular sections of the code may execute after one 
+# another thats why labels are used to jump from one section to another). But when you run make with install as the parameter, the make utility 
+# searches for a label named install within the Makefile, and executes only that section of the Makefile.
+# The install section happens to be only a part where the executables and other required files created during the last step (i.e. make) are copied 
+# into the required final directories on your machine. E.g. the executable that the user runs may be copied to the /usr/local/bin so that all users 
+# are able to run the software. Similarly all the other files are also copied to the standard directories in Linux. Remember that when you ran make, 
+# all the executables were created in the source directory. So when you run make install, these executables are copied to the final directories.
+
+## Donot touch anything below this line
+#x264
+pushd x264
+make distclean
 
 # Here we are configuring x264, you can use "cd x264 && ./configure --help" to check various configuration options for x264 library
 #./configure \
@@ -42,6 +67,7 @@ make distclean
 --enable-static \
 --disable-cli
 
+# building x264 where -j$NUMBER_OF_CORES speeds up the build by using multiple processor cores
 make -j$NUMBER_OF_CORES
 popd
 
