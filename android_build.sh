f040f25a33a8f5411b5c6e366fe0671b8663fa6b
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2014-07-25 09:53:18 -0400

    removed comments

--- android_build.sh
+++ android_build.sh
@@ -1,63 +1,14 @@
 #!/bin/bash
 
-## Configure your settings here
 ANDROID_NDK_ROOT_PATH=~/Android/ndk
 ANDROID_API_VERSION=android-9
-# including android toolchain binaries by exporting toolchains bin path to PATH variable
 export PATH=${ANDROID_NDK_ROOT_PATH}:${ANDROID_NDK_ROOT_PATH}/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/bin/:$PATH
-# sysroot is a GCC term for a directory containing the system headers and libraries of your target
 NDK_SYSROOT=$ANDROID_NDK_ROOT_PATH/platforms/$ANDROID_API_VERSION/arch-arm
-NUMBER_OF_CORES=$(nproc) # or number of cores you want to use to compile ffmpeg
+NUMBER_OF_CORES=$(nproc)
 
-# Basic compilation in linux
-# ./configure -
-# This command makes the shell run the script named configure which exists in the current directory. The configure script basically 
-# consists of many lines which are used to check some details about the machine on which the software is going to be installed. 
-# This script checks for lots of dependencies on your system. For the particular software to work properly, it may be requiring a 
-# lot of things to be existing on your machine already.  When you run the configure script you would see a lot of output on the screen, 
-# each being some sort of question and a respective yes/no as the reply. If any of the major requirements are missing on your system, 
-# the configure script would exit and you cannot proceed with the installation, until you get those required things. 
-# The main job of the configure script is to create a Makefile. This is a very important file for the installation process. Depending on 
-# the results of the tests (checks) that the configure script performed it would write down the various steps that need to be taken (while 
-# compiling the software) in the file named Makefile.
-
-# make
-# make is actually a utility which exists on almost all Unix systems. For make utility to work it requires a file named Makefile in the same 
-# directory in which you run make. As we have seen the configure script's main job was to create a file named Makefile to be used with make 
-# utility. (Sometimes the Makefile is named as makefile also)
-# make would use the directions present in the Makefile and proceed with the installation. The Makefile indicates the sequence, that Linux 
-# must follow to build various components / sub-programs of your software. The sequence depends on the way the software is designed as well 
-# as many other factors.
-# The Makefile actually has a lot of labels (sort of names for different sections). Hence depending on what needs to be done the control would 
-# be passed to the different sections within the Makefile Or it is possible that at the end of one of the section there is a command to go to 
-# some next section.
-# Basically the make utility compiles all your program code and creates the executables. For particular section of the program to complete might 
-# require some other part of the code already ready, this is what the Makefile does. It sets the sequence for the events so that your program does 
-# not complain about missing dependencies.
-
-# make install
-# As indicated before make uses the file named Makefile in the same directory. When you run make without any parameters, the instruction in the 
-# Makefile begin executing from the start and as per the rules defined within the Makefile (particular sections of the code may execute after one 
-# another thats why labels are used to jump from one section to another). But when you run make with install as the parameter, the make utility 
-# searches for a label named install within the Makefile, and executes only that section of the Makefile.
-# The install section happens to be only a part where the executables and other required files created during the last step (i.e. make) are copied 
-# into the required final directories on your machine. E.g. the executable that the user runs may be copied to the /usr/local/bin so that all users 
-# are able to run the software. Similarly all the other files are also copied to the standard directories in Linux. Remember that when you ran make, 
-# all the executables were created in the source directory. So when you run make install, these executables are copied to the final directories.
-
-## Donot touch anything below this line
-#x264
+# x264
 pushd x264
 make distclean
-
-# Here we are configuring x264, you can use "cd x264 && ./configure --help" to check various configuration options for x264 library
-#./configure \
-#--cross-prefix=arm-linux-androideabi- \  # Prefix for compilation tools
-#--sysroot="$NDK_SYSROOT" \ # ndk sysroot dir
-#--host=arm-linux \ # host system for which we are compiling
-#--enable-pic \ # build position-independent code
-#--enable-static \ # build static library
-#--disable-cli # disable cli tools
 
 ./configure \
 --cross-prefix=arm-linux-androideabi- \
@@ -67,27 +18,11 @@ make distclean
 --enable-static \
 --disable-cli
 
-# building x264 where -j$NUMBER_OF_CORES speeds up the build by using multiple processor cores
 make -j$NUMBER_OF_CORES
 popd
 
 #ffmpeg armv7-a neon
 pushd ffmpeg
-# here we are configuring ffmpeg for armv7-a neon devices
-#./configure \
-#--target-os=linux \ # we are compiling it on linux
-#--cross-prefix=arm-linux-androideabi- \
-#--arch=arm \ # architecture we are compiling for
-#--cpu=armv7-a \ # cpu we are compiling for
-#--sysroot="$NDK_SYSROOT" \
-#--disable-avdevice \ # disable libavdevice build
-#--disable-decoder=h264_vdpau \ # disabling decoder h264_vdpau
-#--enable-libx264 \  # enabling x264 lib
-#--enable-gpl \
-#--prefix=../build/armeabi-v7a-neon \ # setting build prefix path
-#--extra-cflags='-I../x264 -DANDROID -I${ANDROID_NDK_ROOT_PATH}/sources/cxx-stl/system/include -march=armv7-a -mfloat-abi=softfp -mfpu=neon' \ # setting C Flags for compilation and included x264 library
-#--extra-ldflags='-L../x264 -Wl,--fix-cortex-a8 -L../android-libs -Wl,-rpath-link,../android-libs' \ # setting ldflags for compilation  and included x264 library
-#--extra-cxxflags='-Wno-multichar -fno-exceptions -fno-rtti' # setting cxx compilation flags
 
 ./configure \
 --target-os=linux \
@@ -104,13 +39,11 @@ pushd ffmpeg
 --extra-ldflags='-L../x264 -Wl,--fix-cortex-a8 -L../android-libs -Wl,-rpath-link,../android-libs' \
 --extra-cxxflags='-Wno-multichar -fno-exceptions -fno-rtti'
 
-# building and copying binaries to prefix directory path
 make -j$NUMBER_OF_CORES && make install && make distclean|| exit 0
 popd
 
 #ffmpeg armv7-a
 pushd ffmpeg
-# here we are configuring ffmpeg for armv7-a cpu devices
 ./configure \
 --target-os=linux \
 --cross-prefix=arm-linux-androideabi- \
@@ -126,7 +59,6 @@ pushd ffmpeg
 --extra-ldflags='-L../x264 -Wl,--fix-cortex-a8 -L../android-libs -Wl,-rpath-link,../android-libs' \
 --extra-cxxflags='-Wno-multichar -fno-exceptions -fno-rtti'
 
-# building and copying binaries to prefix directory path
 make -j$NUMBER_OF_CORES && make install && make distclean|| exit 0
 popd
 
@@ -134,5 +66,3 @@ pushd x264
 make distclean
 popd
 
-
-#Ref : http://www.codecoffee.com/tipsforlinux/articles/27.html
