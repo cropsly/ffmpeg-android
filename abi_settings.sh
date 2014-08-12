304986ace6e5c7a177c57fbe72a2955d23911500
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2014-08-12 09:42:05 -0400

    Added settings and LICENSES

---
+++ abi_settings.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+. settings.sh
+
+BASEDIR=$2
+
+case $1 in
+  armeabi-v7a)
+    NDK_ABI='arm'
+    NDK_TOOLCHAIN_ABI='arm-linux-androideabi'
+    ARCH_CPU='armv7-a'
+    CFLAGS="$CFLAGS_LIBS -march=$ARCH_CPU"
+  ;;
+  armeabi-v7a-neon)
+    NDK_ABI='arm'
+    NDK_TOOLCHAIN_ABI='arm-linux-androideabi'
+    ARCH_CPU='armv7-a'
+    CFLAGS="$CFLAGS -march=$ARCH_CPU -mfpu=neon"
+    CFLAGS_LIBS="$CFLAGS_LIBS -mfpu=neon"
+  ;;
+esac
+
+TOOLCHAIN_PREFIX=${BASEDIR}/toolchain-${1}
+if [ ! -d "$TOOLCHAIN_PREFIX" ]; then
+  ${ANDROID_NDK_ROOT_PATH}/build/tools/make-standalone-toolchain.sh --toolchain=${NDK_TOOLCHAIN_ABI}-${NDK_TOOLCHAIN_ABI_VERSION} --platform=android-${ANDROID_API_VERSION} --install-dir=${TOOLCHAIN_PREFIX}
+fi
+CROSS_PREFIX=${TOOLCHAIN_PREFIX}/bin/${NDK_TOOLCHAIN_ABI}-
+NDK_SYSROOT=${TOOLCHAIN_PREFIX}/sysroot
+CC="${CROSS_PREFIX}gcc --sysroot=${NDK_SYSROOT}"
+LD="${CROSS_PREFIX}ld"
+RANLIB="${CROSS_PREFIX}ranlib"
+STRIP="${CROSS_PREFIX}strip"
+READELF="${CROSS_PREFIX}readelf"
+OBJDUMP="${CROSS_PREFIX}objdump"
+ADDR2LINE="${CROSS_PREFIX}addr2line"
+AR="${CROSS_PREFIX}ar"
+AS="${CROSS_PREFIX}as"
+CXX="${CROSS_PREFIX}g++"
+OBJCOPY="${CROSS_PREFIX}objcopy"
+ELFEDIT="${CROSS_PREFIX}elfedit"
+CPP="${CROSS_PREFIX}cpp"
+DWP="${CROSS_PREFIX}dwp"
+GCONV="${CROSS_PREFIX}gconv"
+GDP="${CROSS_PREFIX}gdb"
+GPROF="${CROSS_PREFIX}gprof"
+NM="${CROSS_PREFIX}nm"
+SIZE="${CROSS_PREFIX}size"
+STRINGS="${CROSS_PREFIX}strings"
---
+++ settings.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SUPPORTED_ARCHITECTURES=(armeabi-v7a armeabi-v7a-neon)
+ANDROID_NDK_ROOT_PATH=~/Android/ndk
+ANDROID_API_VERSION=9
+NDK_TOOLCHAIN_ABI_VERSION=4.8
+
+NUMBER_OF_CORES=$(nproc)
+HOST_UNAME=$(uname -m)
+TARGET_OS=linux
+
+CFLAGS='-U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2 -fno-strict-overflow -fstack-protector-all'
+LDFLAGS='-Wl,-z,relro -Wl,-z,now -pie'
+
+CFLAGS_LIBS="-std=gnu99 -mcpu=cortex-a8 -marm -mfloat-abi=softfp -mfpu=neon"
+FFMPEG_PKG_CONFIG="$(pwd)/ffmpeg-pkg-config"
