60449b9eec3cf54aa6169bad1d71bd3efc3e59c0
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2014-08-13 09:01:33 -0400

    updated cflags_libs

--- settings.sh
+++ settings.sh
@@ -12,5 +12,5 @@ TARGET_OS=linux
 CFLAGS='-U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2 -fno-strict-overflow -fstack-protector-all'
 LDFLAGS='-Wl,-z,relro -Wl,-z,now -pie'
 
-CFLAGS_LIBS="-std=gnu99 -mcpu=cortex-a8 -marm -mfloat-abi=softfp -mfpu=neon"
+CFLAGS_LIBS="-std=gnu99 -mcpu=cortex-a8 -marm -mfloat-abi=softfp"
 FFMPEG_PKG_CONFIG="$(pwd)/ffmpeg-pkg-config"
