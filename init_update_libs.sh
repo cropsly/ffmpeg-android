96a974db453fa5e56cc4928b4a43e3879fcc2652
Hitesh Sondhi <hiteshsondhi88@gmail.com>
2015-03-05 12:00:56 +0000

    Updated libpng to 1.6.16

--- init_update_libs.sh
+++ init_update_libs.sh
@@ -9,7 +9,7 @@ rm -rf libpng-*
 rm -rf expat-*
 rm -rf fribidi-*
 
-wget -O- https://downloads.sf.net/project/libpng/libpng16/1.6.13/libpng-1.6.13.tar.xz | tar xJ
+wget -O- https://downloads.sf.net/project/libpng/libpng16/1.6.16/libpng-1.6.16.tar.xz | tar xJ
 wget -O- http://downloads.sourceforge.net/project/expat/expat/2.1.0/expat-2.1.0.tar.gz | tar xz
 wget -O- http://fribidi.org/download/fribidi-0.19.6.tar.bz2 | tar xj
 echo "============================================"
--- libpng_build.sh
+++ libpng_build.sh
@@ -2,7 +2,7 @@
 
 . abi_settings.sh $1 $2 $3
 
-pushd libpng-1.6.13
+pushd libpng-1.6.16
 
 make clean
 
