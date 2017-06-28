207c9423b17c2c10a31dc8750a09af8cf73d4874
Hitesh <hitesh@writingminds.com>
2017-06-28 17:54:04 +0530

    Updated libpng to 1.6.29 and expat to 2.2.1

--- init_update_libs.sh
+++ init_update_libs.sh
@@ -10,8 +10,8 @@ rm -rf expat-*
 rm -rf fribidi-*
 rm -rf lame-*
 
-wget -O- ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/libpng-1.6.21.tar.xz | tar xJ || exit 1
-wget -O- http://downloads.sourceforge.net/project/expat/expat/2.1.0/expat-2.1.0.tar.gz | tar xz || exit 1
+wget -O- ftp://ftp.simplesystems.org/pub/libpng/png/src/libpng16/libpng-1.6.29.tar.xz | tar xJ || exit 1
+wget -O- http://downloads.sourceforge.net/project/expat/expat/2.2.1/expat-2.2.1.tar.bz2 | tar xz || exit 1
 wget -O- http://fribidi.org/download/fribidi-0.19.7.tar.bz2 | tar xj || exit 1
 wget -O- http://sourceforge.net/projects/lame/files/lame/3.99/lame-3.99.5.tar.gz | tar xz || exit 1
 echo "============================================"
